ffbccd13e266562b3d40030fa74c02fb3ff8480a
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-04-15 14:02:11 -0400

    first commit

---
+++ challenges/challenge-1.cs
@@ -0,0 +1,17 @@
+/* Write code that produces the following output:
+
+Output
+This is the first line.
+This is the second line.
+
+In the previous unit, you learned how to display a 
+message in just one line of code, and you learned 
+how to display a message using multiple lines of code. 
+Use both techniques for this challenge. It doesn't 
+matter which technique you apply to which line, and 
+it doesn't matter how many ways you split one of 
+the messages into multiple lines of code. That's your choice. 
+*/
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
