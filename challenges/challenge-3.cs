c332d238398868a9125ea9ca182aeed7136792cf
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-04-18 14:42:09 -0400

    corrected challenge 3

--- challenges/challenge-3.cs
+++ challenges/challenge-3.cs
@@ -33,8 +33,10 @@ View English output:
 string projectName = "ACME";
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
-Console.Write("View English output:\n\t");
-Console.WriteLine($@"c:\Exercise\{projectName}\data.txt");
+string englishMessage = "View English output:";
+string englishPath = $@"c:\Exercise\{projectName}\data.txt";
+string russianPath = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
 
-Console.Write($"\n{russianMessage}:\n\t");
-Console.WriteLine($@"c:\Exercise\{projectName}\ru-RU\data.txt");
+
+Console.WriteLine($"{englishMessage}\n\t{englishPath}\n");
+Console.WriteLine($"{russianMessage}\n\t{russianPath}");
