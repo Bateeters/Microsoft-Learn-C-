e4ac0c3aed234381087c1822033345fe012169f8
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-04-25 16:31:36 -0400

    comment and explain the code and used methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,3 +1,10 @@
-﻿Random dice = new();
+﻿// Create an instance of the Random class named dice
+Random dice = new();
+
+// Use the dice.Next(1, 7) method to assign a random value to an integer named roll
+// the Next() method includes a method signature that accepts two parameters of type int.
+// Used to configure lower and upper boundaries for the random number returned.
 int roll = dice.Next(1,7);
+
+// Use Console.WriteLine() method to print the value of roll.
 Console.WriteLine(roll);
