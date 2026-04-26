5183fae903c1e1ef57f81b1c15295c1ee6ad3ffb
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-04-26 14:36:20 -0400

    updated prizes to utilize else if and updated output to always show total score

--- CsharpProjects/IfStatementProject/Program.cs
+++ CsharpProjects/IfStatementProject/Program.cs
@@ -51,22 +51,34 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
     {
         Console.WriteLine("You rolled triples! +6 bonus to total!");
         total += 6;
-        Console.WriteLine($"Total with bonus: {total}");
     }
     else
     {
         Console.WriteLine("You rolled doubles! +2 bonus to total!");
         total += 2;
-        Console.WriteLine($"Total with bonus: {total}");
     }
-}
 
-
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
+    Console.WriteLine($"Total with bonus: {total}");
 }
 else
 {
-    Console.WriteLine("You lose!");
+    Console.WriteLine($"Total score: {total}");
+}
+
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
