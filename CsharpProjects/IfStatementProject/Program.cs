12c8339561fc07cf06fb60a49975ad2ea0634e79
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-04-26 14:10:17 -0400

    added prompt and starting code for if statement exercise

--- CsharpProjects/IfStatementProject/Program.cs
+++ CsharpProjects/IfStatementProject/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿/*
+
+Write code that generates three random numbers and displays them in output
+
+Here is the starting code for this exercise:
+
+
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+
+
+To begin, you create a new instance of the System.Random class and store a reference to the object in a variable named dice. 
+Then, you call the Random.Next() method on the dice object three times, providing both the lower and upper bounds to restrict 
+the possible values between 1 and 6 (the upper bound is exclusive). You save the three random numbers in the variables roll1, 
+roll2, and roll3, respectively.
+
+Next, you sum the three dice rolls and save the value into an integer variable named total.
+
+Finally, you use the WriteLine() method to display the three values using string interpolation.
+
+When you run the code, you should see the following message (the numbers will be different).
+
+Output
+
+Dice roll: 4 + 5 + 2 = 11
+
+*/
+
+Random dice = new();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
--- CsharpProjects/IfStatementProject/obj/Debug/net10.0/IfStatementProject.AssemblyInfo.cs
+++ CsharpProjects/IfStatementProject/obj/Debug/net10.0/IfStatementProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IfStatementProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e436f099d49c7fdf69824cae6cdfcfe39aefca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d60a9df3e2f0a6438016003a4ebb1edc56dafe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("IfStatementProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IfStatementProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
