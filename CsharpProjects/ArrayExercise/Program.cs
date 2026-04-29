1870cadd1f838d25e7c816cfb13228968406cae8
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-04-28 20:09:52 -0400

    assign array elements

--- CsharpProjects/ArrayExercise/Program.cs
+++ CsharpProjects/ArrayExercise/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Initialize an array that is of the string data type 
+// called "fraudulentOrderIDs"
+// that can hold 3 string values
+string[] fraudulentOrderIDs = new string[3];
+
+// assign values to the array elements
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+
