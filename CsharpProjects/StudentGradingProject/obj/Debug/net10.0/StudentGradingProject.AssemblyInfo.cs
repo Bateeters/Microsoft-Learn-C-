f4b898607a61e9a1681590c7e9c1e2cca7f29898
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-04-29 21:08:09 -0400

    added support for extra credit and added more students and grades

--- CsharpProjects/StudentGradingProject/obj/Debug/net10.0/StudentGradingProject.AssemblyInfo.cs
+++ CsharpProjects/StudentGradingProject/obj/Debug/net10.0/StudentGradingProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGradingProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808a31187b1be057f12c5217aafe2c57b32bf711")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639673f79d4907ee6b4dc2e63ca2a2228afd681b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGradingProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGradingProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
