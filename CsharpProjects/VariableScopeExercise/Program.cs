6007748cb99dd95d6b814d8c52fa000460e8e07a
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-05-02 15:17:55 -0400

    added code testing variable scope inside and outside if statement

--- CsharpProjects/VariableScopeExercise/Program.cs
+++ CsharpProjects/VariableScopeExercise/Program.cs
@@ -1,6 +1,20 @@
 ﻿bool flag = true;
+
+/*
 if (flag)
 {
     int value = 10;
     Console.WriteLine(value);
 }
+
+// The below line of code causes a compile error because "value" is only defined INSIDE the if block
+// Console.WriteLine(value);
+*/
+
+int value = 10;
+if (flag)
+{
+    Console.WriteLine($"Inside if block: {value}");
+}
+
+Console.WriteLine($"Outside if block: {value}");
