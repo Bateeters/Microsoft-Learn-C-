082a7869435599fa6b86b82dc13d63c8e00e3772
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-05-02 15:54:16 -0400

    added a switch statement

--- CsharpProjects/SwitchStatementExercise/obj/Debug/net10.0/SwitchStatementExercise.AssemblyInfo.cs
+++ CsharpProjects/SwitchStatementExercise/obj/Debug/net10.0/SwitchStatementExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchStatementExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2262398490618b3a26da76ac39903187e2624f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20573df7afc7b2b8b55bb44df3a2b90193df553a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchStatementExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchStatementExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
