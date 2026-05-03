41bcbe2560074274cb33e4f06e8f26a43d1a482d
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-05-03 15:16:23 -0400

    examined foreach statement limitations

--- CsharpProjects/LoopPracticeExercise/Program.cs
+++ CsharpProjects/LoopPracticeExercise/Program.cs
@@ -27,9 +27,28 @@ for (int i = 0; i < 10; i++)
 }
 */
 
+/*
 // Looping through Array
 string[] names = { "Alex", "Eddie", "David", "Michael" };
 for (int i = names.Length - 1; i >= 0; i--) // Reverse loop through "names"
 {
     Console.WriteLine(names[i]);
 }
+*/
+
+/*
+// foreach statement limitation example
+foreach (var name in names)
+{
+    // Can't do this:
+    if (name = "David") name = "Sammy";
+}
+*/
+
+// but we can do it using a for loop
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David") names[i] = "Sammy";
+}
+
+foreach (var name in names) Console.WriteLine(name);
