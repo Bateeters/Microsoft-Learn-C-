abc7adc1e9e16e4d25c2a40ec71b46916d869774
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-05-03 17:46:17 -0400

    finished code exercise 1 of DowhileAndWhileExercises

---
+++ CsharpProjects/DowhileAndWhileExercises/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Code Exercise 1
+Here are the conditions to implement:
+
+Your solution must include either a do-while or while iteration.
+Before the iteration block: your solution must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+Inside the iteration block:
+    Your solution must use a Console.ReadLine() statement to obtain input from the user.
+    Your solution must ensure that the input is a valid representation of an integer.
+    If the integer value isn't between 5 and 10, your code must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+    Your solution must ensure that the integer value is between 5 and 10 before exiting the iteration.
+Below (after) the iteration code block: your solution must use a Console.WriteLine() statement to inform the user that their input value has been accepted.
+
+For example, when you run your application, it should reject input values such as "two" and "2", but it should accept an input value of "7".
+
+The console output should look similar to the following:
+
+Output
+Enter an integer value between 5 and 10
+two
+Sorry, you entered an invalid number, please try again
+2
+You entered 2. Please enter a number between 5 and 10.
+7
+Your input value (7) has been accepted.
+*/
+
+// SOLUTION
+string? readResult;
+int numericValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    readResult = Console.ReadLine();
+    validNumber = int.TryParse(readResult, out numericValue);
+    if (validNumber && numericValue >=5 && numericValue <= 10)
+        continue;
+    else if (validNumber)
+    {
+        Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+        validNumber = false;
+    }
+    else
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numericValue}) has been accepted");
