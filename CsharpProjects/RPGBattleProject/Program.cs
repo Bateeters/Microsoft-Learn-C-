264d0c612a1ac0aa10a6c575b519f7d0a876aafd
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-05-03 16:49:29 -0400

    set up RPGBattleProject

---
+++ CsharpProjects/RPGBattleProject/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Here are the rules for the battle game to implement in your code:
+
+You must use either the do-while statement or the while statement as an outer game loop.
+The hero and the monster start with 10 health points.
+All attacks are a value between 1 and 10.
+The hero attacks first.
+Print the amount of health the monster lost and their remaining health.
+If the monster's health is greater than 0, it can attack the hero.
+Print the amount of health the hero lost and their remaining health.
+Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
+Print the winner.
+
+
+Run your application and verify that your output meets the requirements.
+
+    Monster was damaged and lost 1 health and now has 9 health.
+    Hero was damaged and lost 1 health and now has 9 health.
+    Monster was damaged and lost 7 health and now has 2 health.
+    Hero was damaged and lost 6 health and now has 3 health.
+    Monster was damaged and lost 9 health and now has -7 health.
+    Hero wins!
+
+Since the code uses random numbers and the outcome is different each time, your results are different 
+than the output displayed above. However, you can use this as an example of the output your code should generate.
+*/
