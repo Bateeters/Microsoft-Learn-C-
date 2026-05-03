dbfdc68c595a40e92f26f6bc0dbbc8688821c8eb
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-05-03 15:09:44 -0400

    exploring different counting loops

--- CsharpProjects/LoopPracticeExercise/Program.cs
+++ CsharpProjects/LoopPracticeExercise/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿/*
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+*/
+
+/*
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+*/
+
+/*
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+}
+*/
+
+/*
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+*/
