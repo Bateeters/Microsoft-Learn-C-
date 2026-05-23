e85a7baad86dad54b7b7d1227f50f5d81ba55ff8
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-05-23 12:16:52 -0400

    exploring value reassignment

--- CsharpProjects/ReferenceTypes/Program.cs
+++ CsharpProjects/ReferenceTypes/Program.cs
@@ -3,3 +3,17 @@ data = new int[3];
 
 // or you can do it in one line
 int[] data2 = new int[3];
+
+
+
+string shortenedString = "Hello World!";
+Console.WriteLine(shortenedString);
+
+
+int val_A = 2;
+int val_B = val_A; // val_B is now a copy of val_A, they are independent of each other
+val_B = 5; // changing val_B does not affect val_A, they are value types
+
+Console.WriteLine("\n--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
